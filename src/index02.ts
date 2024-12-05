0f9bd3a1644f422f5e6118f81828c0aaedc7ef74
Joao Otavio <joaootaviocampestrini@gmail.com>
2024-12-04 21:48:12 -0300

    commit inicial

---
+++ src/index02.ts
@@ -0,0 +1,30 @@
+import Scanner from "@codeea/scanner";
+
+async function main() {
+  const scanner = new Scannerner();
+  const menu = `
+    0 - SAIR
+    1 - SOMA
+    2 - SUBTRACAO
+    3 - DIVISAO
+    4 - MULTIPLICACAO
+    5 - POTENCIACAO
+    6 - CHECA SE NUMERO É PAR
+  `;
+
+  let operacao = 0;
+
+  do {
+switch (key) {
+  case value:
+    
+    break;
+
+  default:
+    break;
+}
+  } while (operacao !== 0);
+}
+
+main ();
+
